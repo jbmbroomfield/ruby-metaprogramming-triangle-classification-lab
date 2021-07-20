3bbe3f4e0c8488f2821ac6e6391f496b8624ce0d
jim <jbmbroomfield@gmail.com>
2021-07-20 11:47:12 +1000

    Done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,51 @@
 class Triangle
-  # write code here
+	attr_reader :a, :b, :c
+
+	def initialize(a, b, c)
+		@a = a
+		@b = b
+		@c = c
+	end
+
+	def sides
+		[self.a, self.b, self.c]
+	end
+
+	def unique_sides
+		self.sides.uniq.length
+	end
+
+	def valid?
+		self.sides.min > 0 && self.triangle_inequality
+	end
+
+	def triangle_inequality
+		sides = self.sides
+		max_side = sides.max
+		max_side_index = sides.index(max_side)
+		sides.slice!(max_side_index)
+		max_side < sides.sum
+	end
+
+	def kind
+		if !self.valid?
+			raise TriangleError
+		else
+			check_kind
+		end
+	end
+
+	def check_kind
+		case self.unique_sides
+		when 1
+			:equilateral
+		when 2
+			:isosceles
+		else
+			:scalene
+		end
+	end
+
+	class TriangleError < StandardError
+	end
 end
